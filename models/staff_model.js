a51d9aede364c95919b98425e79ff2a7675c689a
sipho02 <siphojunior569@gmail.comsipho02>
2024-09-13 15:59:35 +0200

    Sipho did the services

--- models/staff_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const staffSchema = new mongoose.Schema({
-    business: {  type:String, ref:"Business",required: true, },
-    name: { type: String, required: true },
-    position: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Staff', staffSchema);
