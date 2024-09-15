d87e3b8fd9d1d821df967a260d6c1edab7aeb533
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-09-15 22:34:41 +0200

    update server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = require('./routes/routes')
 const businessRoutes = require('./routes/business_routes')
+const paymentRoutes = require('./routes/payment_routes');
+const serviceRoutes = require('./routes/service_routes')
 const cors = require('cors');
 const connectToDB = require('./config/config');
 
@@ -12,13 +14,15 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use('/', router);
+// app.use('/', router);
 
 connectToDB();
 
-app.use("/users" , userRoutes);
-app.use("/business", businessRoutes);
-app.use("/staff",router);
+// app.use("/users" , userRoutes);
+// app.use("/business", businessRoutes);
+// app.use("/staff",router);
+app.use('/api', paymentRoutes);
+app.use('/api', serviceRoutes);
 
 app.listen(port, () => {
     console.log(`server is running on port ${3000}`);
