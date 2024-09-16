7e4b50930126d1af82277fca7285e4962b3a27fc
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-09-16 22:16:51 +0200

    add routes

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const router = require('./routes/routes')
 const businessRoutes = require('./routes/business_routes')
 const paymentRoutes = require('./routes/payment_routes');
 const serviceRoutes = require('./routes/service_routes')
+const bookingRoutes = require('./routes/booking_router');
+const staffRoutes = require('./routes/staff_routes');
+const userRoutes = require('./routes/user_routes');
+
 const cors = require('cors');
 const connectToDB = require('./config/config');
 
-const userRoutes = require('./routes/user_routes');
 
 const app = express();
 
@@ -18,11 +21,12 @@ app.use(cors());
 
 connectToDB();
 
-// app.use("/users" , userRoutes);
-// app.use("/business", businessRoutes);
-// app.use("/staff",router);
+app.use("/api" , userRoutes);
+app.use("/api", businessRoutes);
+app.use("/api", staffRoutes);
 app.use('/api', paymentRoutes);
 app.use('/api', serviceRoutes);
+app.use('/api', bookingRoutes);
 
 app.listen(port, () => {
     console.log(`server is running on port ${3000}`);
